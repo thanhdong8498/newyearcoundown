4860f4a92711f02d1ca0bde9aab445845876b836
Nguyễn Thanh Đông <91796684+thanhdong8498@users.noreply.github.com>
2023-01-20 20:25:18 +0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,155 @@
+const targetDate = new Date("Jan 22, 2023 00:00:00");
+const background = document.getElementById('background')
+const countdown = document.getElementById('countdown')
+targetDate.setHours(targetDate.getHours());
+const modal = document.getElementById('modal')
+const answer = document.getElementById('answer');
+const btnSubmit = document.getElementById('btn-submit')
+let completed = false
+let userName = ''
+function getTimeSegmentElements(segmentElement) {
+  const segmentDisplay = segmentElement.querySelector(".segment-display");
+  const segmentDisplayTop = segmentDisplay.querySelector(
+    ".segment-display__top"
+  );
+  const segmentDisplayBottom = segmentDisplay.querySelector(
+    ".segment-display__bottom"
+  );
+
+  const segmentOverlay = segmentDisplay.querySelector(".segment-overlay");
+  const segmentOverlayTop = segmentOverlay.querySelector(
+    ".segment-overlay__top"
+  );
+  const segmentOverlayBottom = segmentOverlay.querySelector(
+    ".segment-overlay__bottom"
+  );
+
+  return {
+    segmentDisplayTop,
+    segmentDisplayBottom,
+    segmentOverlay,
+    segmentOverlayTop,
+    segmentOverlayBottom,
+  };
+}
+
+function updateSegmentValues(displayElement, overlayElement, value) {
+  displayElement.textContent = value;
+  overlayElement.textContent = value;
+}
+
+function updateTimeSegment(segmentElement, timeValue) {
+  const segmentElements = getTimeSegmentElements(segmentElement);
+
+  if (
+    parseInt(segmentElements.segmentDisplayTop.textContent, 10) === timeValue
+  ) {
+    return;
+  }
+
+  segmentElements.segmentOverlay.classList.add("flip");
+
+  updateSegmentValues(
+    segmentElements.segmentDisplayTop,
+    segmentElements.segmentOverlayBottom,
+    timeValue
+  );
+
+  function finishAnimation() {
+    segmentElements.segmentOverlay.classList.remove("flip");
+    updateSegmentValues(
+      segmentElements.segmentDisplayBottom,
+      segmentElements.segmentOverlayTop,
+      timeValue
+    );
+
+    this.removeEventListener("animationend", finishAnimation);
+  }
+
+  segmentElements.segmentOverlay.addEventListener(
+    "animationend",
+    finishAnimation
+  );
+}
+
+function updateTimeSection(sectionID, timeValue) {
+  const firstNumber = Math.floor(timeValue / 10) || 0;
+  const secondNumber = timeValue % 10 || 0;
+  const sectionElement = document.getElementById(sectionID);
+  const timeSegments = sectionElement.querySelectorAll(".time-segment");
+
+  updateTimeSegment(timeSegments[0], firstNumber);
+  updateTimeSegment(timeSegments[1], secondNumber);
+}
+
+function getTimeRemaining(targetDateTime) {
+  const nowTime = Date.now();
+  const complete = nowTime >= targetDateTime;
+
+  if (complete) {
+    let header = document.getElementById("header");
+    header.style.fontSize = "120px";
+    $("section").fireworks({
+      sound: true, // sound effect
+      opacity: 0.9,
+      width: "100%",
+      height: "100%",
+    });
+    background.style.display = 'none'
+    countdown.style.display = 'none'
+    header.innerHTML = 'Chúc mừng năm mới'
+    let wish = document.getElementById('wish')
+    wish.innerHTML = userName
+    completed = true
+    return {
+      complete,
+      seconds: 0,
+      minutes: 0,
+      hours: 0,
+      days: 0,
+    };
+  }
+
+  const secondsRemaining = Math.floor((targetDateTime - nowTime) / 1000);
+  const days = Math.floor(secondsRemaining / (60 * 60 * 24));
+  const hours = Math.floor((secondsRemaining % (60 * 60 * 24)) / (60 * 60));
+  const minutes = Math.floor((secondsRemaining % (60 * 60)) / 60);
+  const seconds = secondsRemaining % 60;
+
+  return {
+    complete,
+    seconds,
+    minutes,
+    hours,
+    days,
+  };
+}
+
+function updateAllSegments() {
+  const timeRemainingBits = getTimeRemaining(new Date(targetDate).getTime());
+
+  updateTimeSection("seconds", timeRemainingBits.seconds);
+  updateTimeSection("minutes", timeRemainingBits.minutes);
+  updateTimeSection("hours", timeRemainingBits.hours);
+  updateTimeSection("days", timeRemainingBits.days);
+
+  return timeRemainingBits.complete;
+}
+
+const countdownTimer = setInterval(() => {
+  const isComplete = updateAllSegments();
+
+  if (isComplete) {
+    clearInterval(countdownTimer);
+  }
+}, 1000);
+
+updateAllSegments();
+
+btnSubmit.addEventListener("click",function(){
+  modal.classList.add('hide')
+  userName = answer.value
+  if(completed==true){
+    document.getElementById('wish').innerHTML = userName;
+  }
+})
